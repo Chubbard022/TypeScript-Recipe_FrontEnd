e91808a8c61e68dc49ddd2ee905673c7f29e8ab0
Chubbard022 <chubbard022@gmail.com>
2020-10-21 09:16:14 -0700

    working on getting redux application set up

--- typedcript_fe/src/customComponents/Form.tsx
+++ typedcript_fe/src/customComponents/Form.tsx
@@ -4,32 +4,32 @@ import { ReactNode } from "react";
 
 
 
-interface FormProps{
-    //children?:ReactNode,
-    username: string,
-    password: string
-}
+// interface FormProps{
+//     //children?:ReactNode,
+//     username: string,
+//     password: string
+// }
 
 
 const handleInputChange = (input:string) =>{
     //based on user input, set changes to username and password
 }
 
-export const Form:React.FC<FormProps> = (props:FormProps) =>{
+export const Form:React.FC =() =>{
     return(
         <form>
             <input 
             placeholder="username"
             type="text"
             name="usename"
-            value={props.username}
+            //value={props.username}
             onChange={()=>handleInputChange}
             />
             <input 
             placeholder="password"
             type="password"
             name="password"
-            value={props.password}
+            //evalue={props.password}
             onChange={()=>handleInputChange}
             />
             <button>Register</button>
