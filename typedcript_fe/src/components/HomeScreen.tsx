574c0ebe28d83b4b05192845d7c3c96879438794
Chubbard022 <chubbard022@gmail.com>
2020-10-23 08:23:13 -0700

    creating simple input with hooks and typescript

--- typedcript_fe/src/components/HomeScreen.tsx
+++ typedcript_fe/src/components/HomeScreen.tsx
@@ -3,12 +3,9 @@ import {Form} from "../customComponents/Form";
 
 export const HomeScreen:React.SFC = () =>{
     
-    const getUserInput = (e:string) =>{
-        console.log(e)
-    }
     return(
         <div>
-            "WORKING"
+            <p>Todo Appilication</p>
             <Form/>
         </div>
     )
